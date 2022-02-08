8bf1a46170f5ec6bc84fabb7da6f1b2663498142
Letaquin <gunas.valter@gmail.com>
2022-02-09 00:57:59 +0100

    added addons

---
+++ addons.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#software from Arch Linux repositories
+sudo pacman -S --noconfirm --needed kodi
+sudo pacman -S --noconfirm --needed extra/libreoffice-still-fr
+sudo pacman -S --noconfirm --needed sublime-text-4
+sudo pacman -S --noconfirm --needed telegram-desktop
+sudo pacman -S --noconfirm --needed vlc
+
+
+#software from 
