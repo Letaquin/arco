2c7168be31453b902c58db602896f49d65312b4c
Letaquin <gunas.valter@gmail.com>
2022-03-16 01:36:21 +0100

    add script for hp printer

--- addons.sh
+++ addons.sh
@@ -6,6 +6,6 @@ sudo pacman -S --noconfirm --needed extra/libreoffice-still-fr
 sudo pacman -S --noconfirm --needed sublime-text-4
 sudo pacman -S --noconfirm --needed telegram-desktop
 sudo pacman -S --noconfirm --needed vlc
-
+sudo pacman -S --noconfirm --needed hplip
 
 #software from 
