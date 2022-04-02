1c235867ee9862d112ebec72c3509502856b23a9
Letaquin <gunas.valter@gmail.com>
2022-04-03 01:36:55 +0200

    add drivers if kodi crashes

--- addons.sh
+++ addons.sh
@@ -12,3 +12,9 @@ sudo pacman -S --noconfirm --needed hplip
 yay android-udev
 yay droicam
 yay android-tools
+
+#drivers if kodi crashed error :  /usr/bin/kodi: line 186: 28232 Segmentation fault      (core dumped) ${KODI_BINARY} $SAVED_ARGS
+#sudo pacman -S intel-media-driver libva-intel-driver
+#sudo pacman -S libva-mesa-driver mesa-vdpau
+#if nvidia drivers
+#sudo pacman -S nvidia-utils
