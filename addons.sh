34ecf985b279adfbd1c153194d8dc5b785ff4db7
Letaquin <gunas.valter@gmail.com>
2022-04-06 01:13:32 +0200

    add arco-dev script

--- addons.sh
+++ addons.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 #software from Arch Linux repositories
-sudo pacman -S --noconfirm --needed kodi
+sudo pacman -S --noconfirm --needed caffeine
 sudo pacman -S --noconfirm --needed extra/libreoffice-still-fr
+sudo pacman -S --noconfirm --needed hplip
+sudo pacman -S --noconfirm --needed kodi
+sudo pacman -S --noconfirm --needed meld
+sudo pacman -S --noconfirm --needed ripgrep
 sudo pacman -S --noconfirm --needed sublime-text-4
 sudo pacman -S --noconfirm --needed telegram-desktop
-sudo pacman -S --noconfirm --needed vlc
-sudo pacman -S --noconfirm --needed hplip
-sudo pacman -S --noconfirm --needed caffeine
+sudo pacman -S --noconfirm --needed the_platinum_searcher
 sudo pacman -S --noconfirm --needed ulauncher
+sudo pacman -S --noconfirm --needed vlc
 
 #git clone
 #dark theme for caffeine
@@ -16,9 +19,9 @@ sudo pacman -S --noconfirm --needed ulauncher
 
 
 #software for droidcam
+yay android-tools
 yay android-udev
 yay droicam
-yay android-tools
 
 #drivers if kodi crashed error :  /usr/bin/kodi: line 186: 28232 Segmentation fault      (core dumped) ${KODI_BINARY} $SAVED_ARGS
 sudo pacman -S intel-media-driver libva-intel-driver
